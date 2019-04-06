db033aec04257da4347d9a875bdd69ddd74edc84
Aryan7057 <48497076+Aryan7057@users.noreply.github.com>
2019-04-06 07:16:31 +0530

    Adding One More Output On Project

--- myfirstproject.java
+++ myfirstproject.java
@@ -1,7 +1,8 @@
 class myfirstproject
 {
-public static void main(String args[])
-{
-System.out.println("My First Project On GitHub :-)");
-}
+  public static void main(String args[])
+   {
+      System.out.println("My First Project On GitHub :-)");
+      System.out.println("Secound Time Visited On Project :-)");
+   }
 }
